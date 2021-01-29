9704446571971482265355af121332a9c462f2d5
zealotand <49332945+zealotand@users.noreply.github.com>
2021-01-29 22:28:20 +0800

    Hello world

---
+++ source_code/ConflictSuggestion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConflictSuggestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
