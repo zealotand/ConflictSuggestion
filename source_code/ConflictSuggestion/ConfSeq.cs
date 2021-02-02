5eee25fa798bfd3b9a010bcbfa887c80a1ad4437
zealotand <49332945+zealotand@users.noreply.github.com>
2021-02-02 22:07:07 +0800

    Add Some Files At 2021//2//2

---
+++ source_code/ConflictSuggestion/ConfSeq.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConflictSuggestion
+{
+    class ConfSeq
+    {
+    }
+}
---
+++ source_code/ConflictSuggestion/ConfSolu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConflictSuggestion
+{
+    interface IConfSolu
+    {
+
+    }
+
+    class DefaultToLargerPriority:IConfSolu
+    {
+
+    }
+
+    class DefaultToSpecificPrioCont:IConfSolu
+    {
+
+    }
+}
